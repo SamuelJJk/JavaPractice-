a793b3e634aec597ffa4fd6bfbe433e81ac342ed
SamuelJJk <samueljean.j24@gmail.com>
2024-06-10 13:03:58 -0400

    creating classes and and compiling the code

--- Review.java
+++ Review.java
@@ -2,5 +2,17 @@ public class Review {
     public static void main(String[] args) {
         String name = "Sam";
         System.out.println(name);
+        // this is to class my other class
+        person1.main(new String[0]);
+    }
+}
+
+class person1 {
+    public static void main(String[] args) {
+        String person1 = "Jimmy";
+        int age = 24;
+        double height = 180.4;
+        System.out.println("Hi my name is " + person1 + " and I'm " + age + " years old.");
+        System.out.print("I'm " + height + " cm tall");
     }
 }
